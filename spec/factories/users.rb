77577c6a2f29cdf0356b99a8f1c702e18383bff3
David Gustafson <git@radicalskeptic.net>
2014-06-17 11:17:16 -0700

    Adds initial tests for user model.  Configures factory for user model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence(:user_label) do
+    |n| "user#{n}"
+  end
+
+  sequence(:email) { |n| "user#{n}@test.com" }
+  sequence(:user_name) { |n| "user#{n}" }
+
   factory :user do
+    email
+    user_name
+    password "asdfasdf"
+    password_confirmation { |user| user.password }
+    after(:build) { |user| user.skip_confirmation! }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a valid factory' do
+    expect( build(:user) ).to be_valid
+  end
+
+  it 'is invalid without email' do
+    user = build(:user, email: nil)
+    expect( user ).not_to be_valid
+  end
+
+  it "is invalid without username" do
+    user = build(:user, user_name: nil)
+    expect( user ).not_to be_valid
+  end
+
+  it "is invalid with duplicate username" do
+    create(:user, user_name: "bob")
+    user2 = build(:user, user_name: "bob")
+    expect( user2 ).not_to be_valid
+  end
+
+  it "is invalid with duplicate email" do
+    create(:user, email: "bob@test.com")
+    user2 = build(:user, email: "bob@test.com")
+    expect( user2).not_to be_valid
+  end
 end
