f4c2cc10a753f2c23b52f5a193d8d25ec1038e89
David Gustafson <git@radicalskeptic.net>
2014-06-17 20:37:48 -0700

    Changes seeds use active record rather than FactoryGirl

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-FactoryGirl.create(:user,
-                   email: "admin@test.com",
-                   user_name: "admin",
-                   password: ENV['ADMIN_PASSWORD']
-                  )
+#FactoryGirl.create(:user,
+                   #email: "admin@test.com",
+                   #user_name: "admin",
+                   #password: ENV['ADMIN_PASSWORD']
+                  #)
 
-FactoryGirl.create(:user,
-                   email: "test@test.com",
-                   user_name: "test",
-                   password: ENV['TEST_USER_PASSWORD']
-                  )
+#FactoryGirl.create(:user,
+                   #email: "test@test.com",
+                   #user_name: "test",
+                   #password: ENV['TEST_USER_PASSWORD']
+                  #)
+
+User.create(
+             email: "admin@test.com",
+             user_name: "admin",
+             password: ENV['ADMIN_PASSWORD'],
+             password: ENV['ADMIN_PASSWORD'],
+             confirmed_at: Time.now )
+
+User.create(
+             email: "test_user@test.com",
+             user_name: "test_user",
+             password: ENV['ADMIN_PASSWORD'],
+             password: ENV['ADMIN_PASSWORD'],
+             confirmed_at: Time.now )
