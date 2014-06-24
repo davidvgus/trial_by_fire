5b4d8da75907cdbd2e09a20646f4912a9cce9225
David Gustafson <git@radicalskeptic.net>
2014-06-23 22:17:50 -0700

    Adds feature specs for essay crud.

--- spec/features/authenticated_user_can_interact_with_essay_links_spec.rb
+++ spec/features/authenticated_user_can_interact_with_essay_links_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 feature "Authenticated user" do
+  scenario "can view essay links on menubar" do
+    user = create(:user)
+
+    sign_in(user)
+    visit root_path
+
+    within(".nav") do
+      expect(page).to have_content("New Essay")
+      expect(page).to have_content("List Essays")
+    end
+  end
+
   scenario "can click on New Essay link and fill out and submit form" do
     user = create(:user)
 
@@ -20,31 +32,108 @@ feature "Authenticated user" do
     expect(page).to have_content("Test Title")
     expect(page).to have_content("Test Description")
     expect(page).to have_content("Test Body")
-
   end
 
-  scenario "can view essay links on menubar" do
+
+  scenario "view list of own essays" do
     user = create(:user)
+
+    user_essays = {}
+    3.times do |n|
+      user_essays[n] = create(:essay, user: user)
+    end
+
+    other_users_essays = {}
+    3.times do |n|
+      other_users_essays[n] = create(:essay)
+    end
 
     sign_in(user)
     visit root_path
 
     within(".nav") do
-      expect(page).to have_content("New Essay")
-      expect(page).to have_content("List Essays")
+      click_link("List Essays")
+    end
+
+    expect(page).to have_content(user_essays[0].title)
+    expect(page).to have_content(user_essays[1].title)
+    expect(page).to have_content(user_essays[2].title)
+    expect(page).not_to have_content(other_users_essays[0].title)
+    expect(page).not_to have_content(other_users_essays[1].title)
+    expect(page).not_to have_content(other_users_essays[2].title)
+
+  end
+
+  context "sees edit link on" do
+    scenario "index page if essay is not published" do
+      user = create(:user)
+      essay = create(:essay, user: user)
+      sign_in(user)
+
+      visit user_essays_path(user)
+
+      expect(page).to have_content(essay.title)
+
+      expect(page).to have_link("Edit")
+    end
+
+    scenario "show page if essay is not published" do
+      user = create(:user)
+      essay = create(:essay, user: user)
+      sign_in(user)
+
+      visit user_essay_path(user, essay)
+
+      expect(page).to have_content(essay.title)
+      within("#bottom_menu") do
+        expect(page).to have_content("Edit")
+      end
     end
   end
 
-  context "can click through menu to" do
-    scenario "view list of own essays"
+  context "does not see edit link on" do
+    scenario "index page if essay is published" do
+      user = create(:user)
+      essay = create(:essay, user: user)
+      essay.published!
+      sign_in(user)
 
-    scenario "create new essay form"
+      visit user_essays_path(user)
+
+      expect(page).to have_content(essay.title)
+
+      expect(page).not_to have_link("Edit")
+    end
+
+    scenario "show page if essay is published" do
+      user = create(:user)
+      essay = create(:essay, user: user)
+      essay.published!
+      sign_in(user)
+
+      visit user_essay_path(user, essay)
+
+      expect(page).to have_content(essay.title)
+
+      within("#bottom_menu") do
+        expect(page).not_to have_content("Edit")
+      end
+    end
   end
 
-  context "when views essays page" do
+  context "does not see destroy link on" do
+    scenario "index page if essay is published" do
+      user = create(:user)
+      essay = create(:essay, user: user)
+      essay.published!
+      sign_in(user)
 
-    scenario "can not edit published essays"
+      visit user_essays_path(user)
 
+      expect(page).to have_content(essay.title)
+
+      expect(page).not_to have_link("Destroy")
+    end
   end
 
 end
