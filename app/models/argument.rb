1346e42f21015ed3f57cb3cd17a66375985eef8c
David Gustafson <git@radicalskeptic.net>
2014-06-25 00:03:03 -0700

    Argument belongs to user as both owner and con_side and both ids are required.

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -1,4 +1,11 @@
 class Argument < ActiveRecord::Base
 
+  belongs_to :owner, class_name: "User"
+  belongs_to :con_side, class_name: "User"
+
+  validates_presence_of :owner_id
+  validates_presence_of :con_side_id
+
   enum status: { :submitted => 0, :under_consideration => 1, :complete => 2 }
+
 end
