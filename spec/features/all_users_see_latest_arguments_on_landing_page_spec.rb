a334ddc7980e21665d813638f6dda9f2191568f7
David Gustafson <git@radicalskeptic.net>
2014-06-25 00:04:58 -0700

    Begins feature testing for argument listing on landing page.

--- spec/features/all_users_see_latest_arguments_on_landing_page_spec.rb
+++ spec/features/all_users_see_latest_arguments_on_landing_page_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
-feature "Latest Arguments appear on the landing page for" do
-  scenario "guests"
 
-  scenario "authenticated users"
+feature "Latest Arguments appear on the landing page for" do
+  before(:each) do
+      @arguments = []
+      3.times do |n|
+        @arguments << create(:argument)
+      end
+  end
+  scenario "guests" do
+
+    visit root_path
+
+    within('#argument_listing') do
+      expect(page).to have_content(@arguments[0].title)
+      expect(page).to have_content(@arguments[1].title)
+      expect(page).to have_content(@arguments[2].title)
+    end
+  end
+
+  scenario "authenticated users" do
+    user = create(:user)
+    sign_in(user)
+
+    within('#argument_listing') do
+      expect(page).to have_content(@arguments[0].title)
+      expect(page).to have_content(@arguments[1].title)
+      expect(page).to have_content(@arguments[2].title)
+    end
+
+  end
 end
