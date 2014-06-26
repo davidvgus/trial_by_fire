93acd2aa20b9b1bf0a9646efb890f5eb988c67d0
David Gustafson <git@radicalskeptic.net>
2014-06-26 11:36:16 -0700

    Adds test that users see their conside arguments on landing page.

--- spec/features/authenticated_users_can_create_arguments_spec.rb
+++ spec/features/authenticated_users_can_create_arguments_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 feature "users" do
+
+  context "see, on the arguments index page," do
+    scenario "a list of unused arguments."
+    scenario "a list of used (published) arguments."
+  end
+
   scenario "who are authed can see My Arguments link on menu" do
     user = create(:user)
     sign_in(user)
@@ -10,19 +16,22 @@ feature "users" do
       expect(page).to have_content("My Arguments")
     end
   end
-  context "who are authed can click on argument link in menubar and " do
+  context "who are authed can click on arguments link in menubar and " do
     before(:each) do
       @user = create(:user)
       sign_in(@user)
     end
 
-    scenario "see arguments they created" do
+    scenario "see only arguments they created in .owned_arguments div" do
       own_arguments = []
       3.times do |n|
         own_arguments << create(:argument, owner_id: @user.id)
       end
 
-      #make arguments user doesn't own
+      other_arguments = []
+      3.times do |n|
+        other_arguments << create(:argument)
+      end
 
       visit root_path
       click_link("My Arguments")
@@ -31,12 +40,46 @@ feature "users" do
         expect(page).to have_content(own_arguments[0].essay.title)
         expect(page).to have_content(own_arguments[1].essay.title)
         expect(page).to have_content(own_arguments[2].essay.title)
+        expect(page).not_to have_content(other_arguments[0].essay.title)
+        expect(page).not_to have_content(other_arguments[1].essay.title)
+        expect(page).not_to have_content(other_arguments[2].essay.title)
+      end
+    end
+
+    scenario "see arguments they are con_sides for" do
+      own_arguments = []
+      3.times do |n|
+        own_arguments << create(:argument, owner_id: @user.id)
       end
 
-      #test that non owned args don't appear on page
+      other_arguments = []
+      3.times do |n|
+        other_arguments << create(:argument)
+      end
+
+      con_side_arguments = []
+      3.times do |n|
+        con_side_arguments << create(:argument, con_side_id: @user.id)
+      end
+
+      visit root_path
+      click_link("My Arguments")
+
+      within('.con_side_arguments') do
+        expect(page).to have_content(con_side_arguments[0].essay.title)
+        expect(page).to have_content(con_side_arguments[1].essay.title)
+        expect(page).to have_content(con_side_arguments[2].essay.title)
+
+        expect(page).not_to have_content(own_arguments[0].essay.title)
+        expect(page).not_to have_content(own_arguments[1].essay.title)
+        expect(page).not_to have_content(own_arguments[2].essay.title)
+
+        expect(page).not_to have_content(other_arguments[0].essay.title)
+        expect(page).not_to have_content(other_arguments[1].essay.title)
+        expect(page).not_to have_content(other_arguments[2].essay.title)
+      end
 
     end
-    scenario "see arguments they are cons_sides for"
     scenario "see arguments they are judging"
   end
 
