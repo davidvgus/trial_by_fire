7d58a5ea9a33a3b2f148db93911ea6448178d74e
David Gustafson <git@radicalskeptic.net>
2014-06-30 10:37:00 -0700

    Adds feature spec for testing argment completion.

--- spec/features/argument_completes_on_last_vote_spec.rb
+++ spec/features/argument_completes_on_last_vote_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rails_helper'
 
 feature "As a judge" do
@@ -7,15 +6,22 @@ feature "As a judge" do
     @con_side_user = create( :user, user_name: "Con Side User")
     @argument = create(:argument, :being_judged, con_side: @con_side_user, owner: @owner_user)
     @witness = create(:user)
-    Argument::BASE_NUM_JUDGES.times do |n|
-      @argument.judges << create(:user, user_name: "Judge_#{n}")
-    end
-    @judge = create(:user)
-    @argument.judges << @judge
-    sign_in(@judge)
+    @judge1 = create(:user)
+    @argument.judges << @judge1
+    @judge2 = create(:user)
+    @argument.judges << @judge2
+    @judge3 = create(:user)
+    @argument.judges << @judge3
+    @judge4 = create(:user)
+    @argument.judges << @judge4
+    @judge5 = create(:user)
+    @argument.judges << @judge5
+    @judge6 = create(:user)
+    @argument.judges << @judge6
+    sign_in(@judge1)
   end
 
-  scenario "user can cast up-vote" do
+  scenario "Argument is comleted when all judges have voted" do
     visit argument_path(@argument)
     within(".voting_booth") do
       click_on("Affirm the motion")
@@ -28,5 +34,86 @@ feature "As a judge" do
       expect(page).to have_content("Undecided: 5")
       expect(page).to have_content("Total: 6")
     end
+
+    sign_out
+
+    sign_in(@judge2)
+    visit argument_path(@argument)
+    within(".voting_booth") do
+      click_on("Affirm the motion")
+    end
+
+    #@argument.reload
+    within(".vote_tally") do
+      expect(page).to have_content("Affirmative: 2")
+      expect(page).to have_content("Negative: 0")
+      expect(page).to have_content("Undecided: 4")
+      expect(page).to have_content("Total: 6")
+    end
+
+    sign_out
+
+    sign_in(@judge3)
+    visit argument_path(@argument)
+    within(".voting_booth") do
+      click_on("Affirm the motion")
+    end
+
+    #@argument.reload
+    within(".vote_tally") do
+      expect(page).to have_content("Affirmative: 3")
+      expect(page).to have_content("Negative: 0")
+      expect(page).to have_content("Undecided: 3")
+      expect(page).to have_content("Total: 6")
+    end
+
+    sign_out
+
+    sign_in(@judge4)
+    visit argument_path(@argument)
+    within(".voting_booth") do
+      click_on("Affirm the motion")
+    end
+
+    #@argument.reload
+    within(".vote_tally") do
+      expect(page).to have_content("Affirmative: 4")
+      expect(page).to have_content("Negative: 0")
+      expect(page).to have_content("Undecided: 2")
+      expect(page).to have_content("Total: 6")
+    end
+
+    sign_out
+
+    sign_in(@judge5)
+    visit argument_path(@argument)
+    within(".voting_booth") do
+      click_on("Affirm the motion")
+    end
+
+    #@argument.reload
+    within(".vote_tally") do
+      expect(page).to have_content("Affirmative: 5")
+      expect(page).to have_content("Negative: 0")
+      expect(page).to have_content("Undecided: 1")
+      expect(page).to have_content("Total: 6")
+    end
+    sign_out
+
+    sign_in(@judge6)
+    visit argument_path(@argument)
+    within(".voting_booth") do
+      click_on("Affirm the motion")
+    end
+
+    #@argument.reload
+    within(".vote_tally") do
+      expect(page).to have_content("Affirmative: 6")
+      expect(page).to have_content("Negative: 0")
+      expect(page).to have_content("Undecided: 0")
+      expect(page).to have_content("Total: 6")
+    end
+    @argument.reload
+    expect(@argument.complete?).to eql true
   end
 end
