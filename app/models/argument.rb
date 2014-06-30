60611774fb70c09fed9ae6b6979aac6d57d8761c
David Gustafson <git@radicalskeptic.net>
2014-06-30 14:29:15 -0700

    Adds set_winner_and_loser and outcome_message to argument.

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -13,6 +13,8 @@ class Argument < ActiveRecord::Base
 
   enum status: { being_critiqued: 0, selecting_judges: 10, being_judged: 20, complete: 30}
 
+  enum outcome: { undecided: 0, affirmed: 1, negated: 2, tied: 3}
+
   BASE_NUM_JUDGES = 5
 
   def has_judge?(user)
@@ -50,7 +52,31 @@ class Argument < ActiveRecord::Base
     undecided_votes = self.judges.count - (affirmative_votes + negative_votes)
     if self.status == "being_judged" && undecided_votes == 0
       self.complete!
+      self.set_winner_and_loser
     end
+  end
+
+  def set_winner_and_loser
+    if self.up_votes > self.down_votes
+      self.winner_id = self.owner_id
+      self.loser_id = self.con_side_id
+      self.affirmed!
+    elsif self.up_votes < self.down_votes
+      self.winner_id = self.con_side_id
+      self.loser_id = self.owner_id
+      self.negated!
+    else
+      self.tied!
+    end
+  end
+
+  def outcome_message
+    outcome_messages = {"affirmed" => "The Motion is confirmed!",
+      "negated" => "The motion is negated!",
+      "tied" => "The motion is neither confrimed nor negated."
+    }
+
+    outcome_messages[self.outcome]
   end
 
 end
